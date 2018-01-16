5b63e9bfb28504ba796fcd69be85d2fd3781917d
Z94 <zf0zero@gmail.com>
2018-01-17 00:03:08 +0800

    add axios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,16 @@
 <template>
-	<div>
-		
-	</div>
-	<div id="container">
-		<el-form :rules="rules" ref="loginForm" label-width="100px">
-			<el-form-item label="用户名" prop="userName">
-				<el-input v-model="loginForm.userName"></el-input>
-			</el-form-item>
-			<el-form-item label="密码" prop="password">
-				<el-input v-model="loginForm.password"></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="submitForm">登录</el-button>
-			    <el-button @click="resetForm">重置</el-button>
-			</el-form-item>
-		</el-form>
-	</div>
+	<el-form class="container" :rules="rules" ref="loginForm" label-width="100px">
+		<el-form-item label="用户名" prop="userName">
+			<el-input v-model="loginForm.userName"></el-input>
+		</el-form-item>
+		<el-form-item label="密码" prop="password">
+			<el-input v-model="loginForm.password"></el-input>
+		</el-form-item>
+		<el-form-item>
+			<el-button type="primary" @click="submitForm">登录</el-button>
+		    <el-button @click="resetForm">重置</el-button>
+		</el-form-item>
+	</el-form>
 </template>
 <script>
 	export default {
@@ -37,6 +32,15 @@
 		},
 		methods: {
 			submitForm(){
+				this.$axios.post('/login', this.loginForm)
+				.then(function(response){
+
+				})
+				.catch(function(response){
+					console.log(this)
+					console.log(response)
+
+				})
 				if (this.loginForm.userName === 'admin' && this.loginForm.password === 'admin') {
 					console.log('success');
 					this.$router.push('HelloWorld')
@@ -48,3 +52,10 @@
 		}
 	}
 </script>
+<style>
+	.container {
+		margin: 300px auto; 
+		width: 40%;
+		border-radius: 5px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// vue
 import Vue from 'vue'
-import App from './App'
+// vue-router
 import router from './router'
+// element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// axios
+import axios from 'axios'
+// components
+import App from './App'
+
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
 
+Vue.prototype.$axios = axios.create({
+	baseURL: '/api'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
